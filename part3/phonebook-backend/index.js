2605ba38dae6ab9ee04ffd6f8feadbcec4c4c71f
Ronald <chiradzaronald6@gmail.com>
2024-01-24 22:44:30 +0200

    PORT env

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const morgan = require('morgan')
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(morgan('tiny'))
